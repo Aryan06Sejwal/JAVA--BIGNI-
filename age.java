dfff62ed4a5751c64131230f38c5db22c200fac9
Aryan-Sejwal <aryansejwal2020@gmail.com>
2026-07-27 23:34:00 +0530

    code

--- age.java
+++ age.java
@@ -1,3 +1,15 @@
+import java.util.*;
+
 public class age {
-    
+    static public void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        int Age=sc.nextInt();
+        if (Age>=18) {
+            System.out.print("Adult");
+
+        }else{
+            System.out.print("Not adult");
+        }
+        
+    }
 }
