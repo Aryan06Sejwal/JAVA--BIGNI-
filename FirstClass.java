365c95b7cdd51d266a22aa13eade07642347a021
Aryan-Sejwal <aryansejwal2020@gmail.com>
2026-07-27 19:58:58 +0530

    first commit

---
+++ FirstClass.java
@@ -0,0 +1,9 @@
+class FirstClass{
+    public static void main(String args[]){
+        //System.out.println("Hello World");
+        int a=10;
+        int b=25;
+        int sum=a+b;
+        System.out.print(sum);
+    }
+}
