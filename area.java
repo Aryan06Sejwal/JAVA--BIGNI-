af1632777e28ba44aec35503ab2956033b06351e
Aryan-Sejwal <aryansejwal2020@gmail.com>
2026-07-27 20:35:12 +0530

    code

--- area.java
+++ area.java
@@ -1,5 +1,12 @@
+import java.util.*;
+
 public class area {
     static public void main(String args[]){
-        
+       Scanner sc=new Scanner(System.in);
+       double r = sc.nextFloat();
+       double Area =(3.14*r*r);
+       System.out.print(Area);
+
+
     }
 }
