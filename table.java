662557ea105661688c1f4491d88e827c87a2b247
Aryan-Sejwal <aryansejwal2020@gmail.com>
2026-07-27 21:00:33 +0530

    code

---
+++ table.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class table {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int mul1 = n * 1;
+        int mul2 = n * 2;
+        int mul3 = n * 3;
+        int mul4 = n * 4;
+        int mul5 = n * 5;
+
+        System.out.println(mul1 + " " + mul2 + " " + mul3 + " " + mul4 + " " + mul5);
+    }
+}
